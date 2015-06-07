f7c3b59a6c80ca60a0de99691ab751caf09bd1a3
shoguone <avlal.nsu@gmail.com>
2015-06-07 09:46:18 +0600

    Хехей! Проверка дублей! %)
    Ну, вот и она.
    и, конечно,
    #ямудакзаливаюврепуготовыйпроект

---
+++ FormOverlapDetection/MainWindow.xaml.cs
@@ -0,0 +1,313 @@
+﻿using FormOverlapDetection.CurrentModel;
+using FormOverlapDetection.MergedModel;
+using FormOverlapDetection.Templates;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FormOverlapDetection
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> v_vs = new List<string>();
+        private List<PrizConverter> result = new List<PrizConverter>();
+		
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            v_vs.Add("ВВ");
+            v_vs.Add("ВВ МВД");
+            v_vs.Add("ПП");
+            v_vs.Add("МЧС");
+            v_vs.Add("СССИ");
+
+            //CurrentSearch();
+            AbsoluteTokensSearch();
+            AbsoluteMilitaryCardsSearch();
+            AbsolutePassportsSearch();
+
+            FillDrafts();
+            
+            ResultsDataGrid.ItemsSource = result;
+        }
+
+        void CurrentSearch()
+        {
+            //List<CurrentModel.PRIZ> curP = new List<CurrentModel.PRIZ>();
+            List<CurrentModel.PRIZ> curP;
+            using (var ctx = new CurrentModelEntities())
+            {
+                curP = ctx.PRIZ
+                    .Where(p => p.N_KOM != "15")
+                    .Join(ctx.kom, p => p.N_KOM, k => k.N_KOM, (p, k) => new { P = p, K = k })
+                    .Where(e => !v_vs.Contains(e.K.V_VS))
+                    .Select(e => e.P)
+                    .ToList();
+            }
+
+            List<MergedModel.PRIZ> mrgdP = new List<MergedModel.PRIZ>();
+            using (var ctx = new MergedModelEntities())
+            {
+                foreach(var p in curP)
+                {
+                    var entity = ctx.PRIZ.FirstOrDefault(e => e.LN_SER == p.LN_SER && e.LN_NUM == p.LN_NUM
+                        && e.kom.N_KOM != "15" && !v_vs.Contains(e.kom.V_VS));
+                    if (entity != default(MergedModel.PRIZ))
+                    {
+                        mrgdP.Add(entity);
+                    }
+                }
+            }
+
+        }
+
+        void AbsoluteTokensSearch()
+        {
+            List<PrizConverter> mergedInMemoryList = new List<PrizConverter>();
+
+            using (var ctx = new CurrentModelEntities())
+            {
+
+                var entities = ctx.PRIZ
+                    .Join(ctx.kom, p => p.N_KOM, k => k.N_KOM, (p, k) => new { P = p, K = k })
+                    .Where(e => e.K.N_KOM != "15" && e.K.V_VS != null && !v_vs.Contains(e.K.V_VS))
+                    .Select(e => new { ID = e.P.ID, Season = "Current", LN = e.P.LN_SER + e.P.LN_NUM })
+                    .ToList();
+
+                mergedInMemoryList.AddRange(entities
+                    .Select(e => new PrizConverter()
+                    {
+                        ID = e.ID,
+                        Season = e.Season,
+                        LN = e.LN
+                    })
+                );
+            }
+
+
+            using (var ctx = new MergedModelEntities())
+            {
+                var entities = ctx.PRIZ
+                    .Where(p => p.kom.N_KOM != "15" && p.kom.V_VS != null && !v_vs.Contains(p.kom.V_VS))
+                    .Select(e => new { ID = e.ID, Season = e.sourceDatabase.dbName, LN = e.LN_SER + e.LN_NUM })
+                    .ToList();
+
+                mergedInMemoryList.AddRange(entities
+                    .Select(e => new PrizConverter()
+                    {
+                        ID = e.ID,
+                        Season = e.Season,
+                        LN = e.LN
+                    })
+                );
+            }
+
+
+            result.AddRange(mergedInMemoryList
+                .GroupBy(p => p.LN)
+                .Where(g => !string.IsNullOrEmpty(g.Key) && g.Count() > 1 && g.Any(e => e.Season == "Current"))
+                .SelectMany(e => e));
+                //.ToList();
+
+        }
+
+        void AbsoluteMilitaryCardsSearch()
+        {
+            List<PrizConverter> mergedInMemoryList = new List<PrizConverter>();
+
+            using (var ctx = new CurrentModelEntities())
+            {
+
+                var entities = ctx.PRIZ
+                    .Where(e => e.N_KOM != "15")
+                    .Select(e => new { ID = e.ID, Season = "Current", VB = e.S_V_BIL + e.N_V_BIL })
+                    .ToList();
+
+                mergedInMemoryList.AddRange(entities
+                    .Select(e => new PrizConverter()
+                    {
+                        ID = e.ID,
+                        Season = e.Season,
+                        VB = e.VB
+                    })
+                );
+            }
+
+
+            using (var ctx = new MergedModelEntities())
+            {
+                var entities = ctx.PRIZ
+                    .Where(p => p.kom.N_KOM != "15")
+                    .Select(e => new { ID = e.ID, Season = e.sourceDatabase.dbName, VB = e.S_V_BIL + e.N_V_BIL })
+                    .ToList();
+
+                mergedInMemoryList.AddRange(entities
+                    .Select(e => new PrizConverter()
+                    {
+                        ID = e.ID,
+                        Season = e.Season,
+                        LN = e.VB
+                    })
+                );
+            }
+
+
+            result.AddRange(mergedInMemoryList
+                .GroupBy(p => p.VB)
+                .Where(g => !string.IsNullOrEmpty(g.Key) && g.Count() > 1 && g.Any(e => e.Season == "Current"))
+                //.ToList()
+                .SelectMany(e => e));
+
+        }
+
+        void AbsolutePassportsSearch()
+        {
+            List<PrizConverter> mergedInMemoryList = new List<PrizConverter>();
+
+            using (var ctx = new CurrentModelEntities())
+            {
+
+                var entities = ctx.PRIZ
+                    .Where(e => e.N_KOM != "15")
+                    .Select(e => new { ID = e.ID, Season = "Current", PP = e.S_PASPORT + e.N_PASPORT })
+                    .ToList();
+
+                mergedInMemoryList.AddRange(entities
+                    .Select(e => new PrizConverter()
+                    {
+                        ID = e.ID,
+                        Season = e.Season,
+                        PP = e.PP
+                    })
+                );
+            }
+
+
+            using (var ctx = new MergedModelEntities())
+            {
+                var entities = ctx.PRIZ
+                    .Where(p => p.kom.N_KOM != "15")
+                    .Select(e => new { ID = e.ID, Season = e.sourceDatabase.dbName, PP = e.S_PASPORT + e.N_PASPORT })
+                    .ToList();
+
+                mergedInMemoryList.AddRange(entities
+                    .Select(e => new PrizConverter()
+                    {
+                        ID = e.ID,
+                        Season = e.Season,
+                        PP = e.PP
+                    })
+                );
+            }
+
+
+            result.AddRange(mergedInMemoryList
+                .GroupBy(p => p.PP)
+                .Where(g => !string.IsNullOrEmpty(g.Key) && g.Count() > 1 && g.Any(e => e.Season == "Current"))
+                //.ToList()
+                .SelectMany(e => e));
+
+        }
+
+        void FillDrafts()
+        {
+            PrizEqualityComparer pec = new PrizEqualityComparer();
+            result = result
+                .Distinct(pec)
+                .ToList();
+
+            foreach (PrizConverter res in result)
+            {
+                if (res.Season == "Current")
+                {
+                    using (var c = new CurrentModelEntities())
+                    {
+                        var e = c.PRIZ.First(p => p.ID == res.ID);
+                        res.D_PASPORT = e.D_PASPORT;
+                        res.D_PRIB = e.D_PRIB;
+                        res.D_ROD = e.D_ROD;
+                        res.FAM = e.FAM;
+                        res.FL_UB = e.FL_UB;
+                        res.IM = e.IM;
+                        res.LN_NUM = e.LN_NUM;
+                        res.LN_SER = e.LN_SER;
+                        res.N_KOM = e.N_KOM;
+                        res.N_PASPORT = e.N_PASPORT;
+                        res.N_V_BIL = e.N_V_BIL;
+                        res.OTCH = e.OTCH;
+                        res.RVK = e.RVK;
+                        res.S_PASPORT = e.S_PASPORT;
+                        res.S_V_BIL = e.S_V_BIL;
+                    }
+                }
+                else
+                {
+                    using (var c = new MergedModelEntities())
+                    {
+                        var e = c.PRIZ.First(p => p.ID == res.ID);
+                        res.D_PASPORT = e.D_PASPORT;
+                        res.D_PRIB = e.D_PRIB;
+                        res.D_ROD = e.D_ROD;
+                        res.FAM = e.FAM;
+                        res.FL_UB = e.FL_UB;
+                        res.IM = e.IM;
+                        res.LN_NUM = e.LN_NUM;
+                        res.LN_SER = e.LN_SER;
+                        res.N_KOM = e.kom.N_KOM;
+                        res.N_PASPORT = e.N_PASPORT;
+                        res.N_V_BIL = e.N_V_BIL;
+                        res.OTCH = e.OTCH;
+                        res.RVK = e.RVK;
+                        res.S_PASPORT = e.S_PASPORT;
+                        res.S_V_BIL = e.S_V_BIL;
+                    }
+                }
+            }
+
+            //Process.Start();
+        }
+
+    }
+
+    public class PrizEqualityComparer : IEqualityComparer<PrizConverter>
+    {
+
+        public bool Equals(PrizConverter p1, PrizConverter p2)
+        {
+            if (p1.ID == p2.ID
+                & p1.Season == p2.Season)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public int GetHashCode(PrizConverter p)
+        {
+            string hCode = p.Season + p.ID;
+            return hCode.GetHashCode();
+        }
+
+    }
+}
+
---
+++ FormOverlapDetection/Templates/PrizConverter.cs
@@ -0,0 +1,155 @@
+﻿using FormOverlapDetection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormOverlapDetection.Templates
+{
+    public class PrizConverter
+    {
+        public int ID { get; set; }
+
+        public string FAM { get; set; }
+        public string IM { get; set; }
+        public string OTCH { get; set; }
+        public DateTime? D_ROD { get; set; }
+        public string RVK { get; set; }
+        public string N_KOM { get; set; }
+        public int? FL_UB { get; set; }
+        public DateTime? D_PRIB { get; set; }
+        public string S_V_BIL { get; set; }
+        public string N_V_BIL { get; set; }
+        public string LN_SER { get; set; }
+        public string LN_NUM { get; set; }
+        public string S_PASPORT { get; set; }
+        public string N_PASPORT { get; set; }
+        public DateTime? D_PASPORT { get; set; }
+
+        public string Season { get; set; }
+        
+        //public string LN { get; set; }
+        public string LN;
+        public string VB;
+        public string PP;
+
+        public PrizConverter()
+        {
+
+        }
+
+        public PrizConverter(CurrentModel.PRIZ cp)
+        {
+            ID = cp.ID;
+            FAM = cp.FAM;
+            IM = cp.IM;
+            OTCH = cp.OTCH;
+            D_ROD = cp.D_ROD;
+            RVK = cp.RVK;
+            N_KOM = cp.N_KOM;
+            FL_UB = cp.FL_UB;
+            D_PRIB = cp.D_PRIB;
+
+            S_V_BIL = cp.S_V_BIL;
+            N_V_BIL = cp.N_V_BIL;
+
+            LN_SER = cp.LN_SER;
+            LN_NUM = cp.LN_NUM;
+
+            S_PASPORT = cp.S_PASPORT;
+            N_PASPORT = cp.N_PASPORT;
+            D_PASPORT = cp.D_PASPORT;
+        }
+
+        public PrizConverter(MergedModel.PRIZ mp)
+        {
+            ID = mp.ID;
+            FAM = mp.FAM;
+            IM = mp.IM;
+            OTCH = mp.OTCH;
+            D_ROD = mp.D_ROD;
+            RVK = mp.RVK;
+            N_KOM = mp.kom.N_KOM;
+            FL_UB = mp.FL_UB;
+            D_PRIB = mp.D_PRIB;
+
+            S_V_BIL = mp.S_V_BIL;
+            N_V_BIL = mp.N_V_BIL;
+
+            LN_SER = mp.LN_SER;
+            LN_NUM = mp.LN_NUM;
+
+            S_PASPORT = mp.S_PASPORT;
+            N_PASPORT = mp.N_PASPORT;
+            D_PASPORT = mp.D_PASPORT;
+        }
+
+        public static CurrentModel.PRIZ Convert(MergedModel.PRIZ mp)
+        {
+            CurrentModel.PRIZ cp = new CurrentModel.PRIZ();
+
+            cp.ID = mp.ID;
+            cp.FAM = mp.FAM;
+            cp.IM = mp.IM;
+            cp.OTCH = mp.OTCH;
+            cp.D_ROD = mp.D_ROD;
+            cp.RVK = mp.RVK;
+            cp.N_KOM = mp.kom.N_KOM;
+            cp.FL_UB = mp.FL_UB;
+            cp.SPEC = mp.SPEC;
+            cp.BRAK = mp.BRAK;
+            cp.SUD = mp.SUD;
+            cp.OBRAZOV = mp.OBRAZOV;
+            cp.PROF_P = mp.PROF_P;
+            cp.NPU = mp.NPU;
+            cp.REZH_KOM = mp.REZH_KOM;
+            cp.GODN = mp.GODN;
+            cp.P_PREDN = mp.P_PREDN;
+            cp.TDT = mp.TDT;
+            cp.F_DOP = mp.F_DOP;
+            cp.N_DOP = mp.N_DOP;
+            cp.D_DOP = mp.D_DOP;
+            cp.D_PRIB = mp.D_PRIB;
+            cp.FL_UV = mp.FL_UV;
+            cp.D_U_UVOL = mp.D_U_UVOL;
+            cp.D_P_UVOL = mp.D_P_UVOL;
+            cp.FL_SOCH = mp.FL_SOCH;
+            cp.D_U_SOCH = mp.D_U_SOCH;
+            cp.HIST = mp.HIST;
+            cp.ROST = mp.ROST;
+            cp.MASSA = mp.MASSA;
+            cp.STAT = mp.STAT;
+            cp.S_V_BIL = mp.S_V_BIL;
+            cp.N_V_BIL = mp.N_V_BIL;
+            cp.PRIM = mp.PRIM;
+            cp.PR_UBIT = mp.PR_UBIT;
+            cp.PRIM_UBIT = mp.PRIM_UBIT;
+            cp.ZREN = mp.ZREN;
+            cp.R_G_U = mp.R_G_U;
+            cp.R_O_G = mp.R_O_G;
+            cp.R_OB = mp.R_OB;
+            cp.H = mp.H;
+            cp.POSTO = mp.POSTO;
+            cp.LN_SER = mp.LN_SER;
+            cp.LN_NUM = mp.LN_NUM;
+            cp.S_PASPORT = mp.S_PASPORT;
+            cp.N_PASPORT = mp.N_PASPORT;
+            cp.D_PASPORT = mp.D_PASPORT;
+            cp.IMEET_RAZR = mp.IMEET_RAZR;
+            cp.IMEET_REB = mp.IMEET_REB;
+            cp.ODIN_ROD = mp.ODIN_ROD;
+            cp.BEZ_ROD = mp.BEZ_ROD;
+            cp.DO_PRIZ = mp.DO_PRIZ;
+            cp.NA_UCHETE = mp.NA_UCHETE;
+            cp.NAVY = mp.NAVY;
+            cp.S_VA = mp.S_VA;
+            cp.N_VA = mp.N_VA;
+            cp.M_ROD = mp.M_ROD;
+            cp.KEM_VIDAN = mp.KEM_VIDAN;
+            cp.TSP = mp.TSP;
+
+            return cp;
+        }
+    }
+}
